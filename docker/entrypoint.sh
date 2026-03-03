c726199603f16be56c8bb4d634e80f6c2c7f6d13
AHiong <bmav.martinez@gmail.com>
2026-03-03 20:31:26 +0800

    Fix Render Gunicorn startup path

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,9 @@ set -e
 
 python apps/ftirui/manage.py migrate --noinput
 
+if [ "$#" -eq 0 ]; then
+  PORT="${PORT:-8000}"
+  set -- gunicorn --chdir /app/apps/ftirui ftirui.wsgi:application --bind "0.0.0.0:${PORT}"
+fi
+
 exec "$@"
